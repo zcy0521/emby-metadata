dcaa155bffcb81dbcf6167ae266cd4f6b38c7afb
zhangcy <zcy0521@gmail.com>
2022-04-18 23:35:34 +0800

    Madonna

--- jav/Madonna.py
+++ jav/Madonna.py
@@ -17,16 +17,22 @@ class Madonna(object):
     def __init__(self, video_no):
         self.video_no = video_no = video_no.lower().replace('-', '')
 
-        # 详情页
-        url = 'https://www.madonna-av.com/works/detail/' + video_no + '/'
-        response = http.get(url)
-        html = response.text
-        soup = BeautifulSoup(html, features="html.parser")
+        # 搜索列表
+        list_url = 'https://www.madonna-av.com/search/list/?q=' + video_no
+        list_response = http.get(list_url, self.headers)
+        list_html = list_response.text
+        list_soup = BeautifulSoup(list_html, features="html.parser")
 
         # poster
-        self.poster_url = soup.find(attrs={"property": "og:image"})['content']
+        self.poster_url = self.site_url.rstrip('/') + list_soup.find('ul', class_="c-works-list").find('img')['src']
         self.poster_name = os.path.basename(self.poster_url)
         self.poster_ext = os.path.splitext(self.poster_name)[1]
+
+        # 详情页
+        url = self.site_url.rstrip('/') + list_soup.find('ul', class_="c-works-list").find('a')['href']
+        response = http.get(url, self.headers)
+        html = response.text
+        soup = BeautifulSoup(html, features="html.parser")
 
         # fanart
         self.fanart_url = self.site_url.rstrip('/') + soup.find('div', class_="c-sample-image").find('img')['src']
@@ -34,11 +40,11 @@ class Madonna(object):
         self.fanart_ext = os.path.splitext(self.fanart_name)[1]
 
     def download_poster(self):
-        response = http.get(self.poster_url)
+        response = http.get(self.poster_url, self.headers)
         return response.content
 
     def download_fanart(self):
-        response = http.get(self.fanart_url)
+        response = http.get(self.fanart_url, self.headers)
         return response.content
 
     def get_poster_ext(self):
