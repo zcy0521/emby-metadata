a47b26d4f11c1246954825f4c2264048b375f185
zcy0521 <zcy0521@gmail.com>
2022-06-13 19:31:48 +0800

    socks代理

--- utils/http.py
+++ utils/http.py
@@ -7,8 +7,8 @@ def get(url, headers):
     session = requests.Session()
 
     proxies = {
-        "http": "socks5://127.0.0.1:1080",
-        "https": "socks5://127.0.0.1:1080",
+        "http": "socks5://192.168.50.254:1080",
+        "https": "socks5://192.168.50.254:1080",
     }
 
     return session.get(url, headers=headers, proxies=proxies)
