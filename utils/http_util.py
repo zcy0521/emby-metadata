b3eacc67fe103102111bb010e4d1497c1f019afd
yang <zcy0521@gmail.com>
2025-04-09 21:42:16 +0800

    非代理下载

--- utils/http_util.py
+++ utils/http_util.py
@@ -1,22 +1,33 @@
 #!/usr/bin/envpython3
 # -*-coding:utf-8-*-
+import urllib3
 from urllib3.contrib.socks import SOCKSProxyManager
-
-# https://urllib3.readthedocs.io/en/stable/advanced-usage.html#socks-proxies
-proxy = SOCKSProxyManager('socks5h://127.0.0.1:1080/')
 
 
 def get(url, headers=None, redirect=True, *, charset='utf8'):
-    # https://urllib3.readthedocs.io/en/stable/user-guide.html#response-content
-    response = proxy.request('GET', url, headers=headers, redirect=redirect)
+    # 非代理方式 https://urllib3.readthedocs.io/en/stable/user-guide.html#response-content
+    response = urllib3.request('GET', url, headers=headers, redirect=redirect)
+
+    # socks理方式 https://urllib3.readthedocs.io/en/stable/advanced-usage.html#socks-proxies
+    # proxy = SOCKSProxyManager('socks5h://127.0.0.1:10800/')
+    # response = proxy.request('GET', url, headers=headers, redirect=redirect)
+
     return response.data.decode(charset, 'ignore')
 
 
 def post(url, fields, headers=None, redirect=True, *, charset='utf8'):
-    response = proxy.request('POST', url, fields=fields, headers=headers, redirect=redirect)
+    response = urllib3.request('POST', url, fields=fields, headers=headers, redirect=redirect)
+
+    # proxy = SOCKSProxyManager('socks5h://127.0.0.1:10800/')
+    # response = proxy.request('GET', url, headers=headers, redirect=redirect)
+
     return response.data.decode(charset, 'ignore')
 
 
 def download(url, headers=None):
-    response = proxy.request('GET', url, headers=headers)
+    response = urllib3.request('GET', url, headers=headers)
+
+    # proxy = SOCKSProxyManager('socks5h://127.0.0.1:10800/')
+    # response = proxy.request('GET', url, headers=headers, redirect=redirect)
+
     return response.data
